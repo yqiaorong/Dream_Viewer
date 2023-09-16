82bb45dfde85f96a41537bbe73583e70c2da41b4
Shannon Yu <yqiaorong@163.com>
2023-09-16 18:56:03 -0400

    update

--- project_directory/github_code/01_eeg_preprocessing/run_all_scripts.py
+++ project_directory/github_code/01_eeg_preprocessing/run_all_scripts.py
@@ -1,7 +1,13 @@
 import os
 
 # =============================================================================
-# THINGS2 EEG preprocessing
+# THINGS1 EEG preprocessing
 # =============================================================================
 for s in range(1,51):
     os.system(f'python ../project_directory/github_code/01_eeg_preprocessing/THINGS1.py --subj {s}')
+
+# =============================================================================
+# THINGS2 EEG preprocessing
+# =============================================================================
+for s in range(1,11):
+    os.system(f'python ../project_directory/github_code/01_eeg_preprocessing/THINGS2.py --subj {s}')
